f2e7f44c73762386ff12c98ca5513cae99c9946f
Emil <emajer32@gmail.com>
2019-02-13 13:06:30 +0100

    Added a Procfile

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const mongoose = require("mongoose");
 const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 const app = express();
-mongoose.connect("mongodb://localhost:27017/blogNew", {
-  useNewUrlParser: true
-});
+// mongoose.connect("mongodb://localhost:27017/blogNew", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-emil:Neurotransgresja32@cluster0-748gv.mongodb.net/blogNew", {useNewUrlParser: true});
+
 
 app.set('view engine', 'ejs');
 
@@ -82,7 +82,11 @@ app.get("/posts/:postId", function(req, res){
 
   });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
